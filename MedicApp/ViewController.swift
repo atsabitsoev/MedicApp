3fafd6deceeb052183b58229facc6c3a359a6f78
Atsamaz <atsabitsoev>
2019-05-03 20:16:36 +0300

    Initial Commit

---
+++ MedicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedicApp
+//
+//  Created by Ацамаз Бицоев on 03/05/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
