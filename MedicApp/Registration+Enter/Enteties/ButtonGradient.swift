189625637e734f8adf57822f2d6e48acb8e3e244
Atsamaz <atsabitsoev>
2019-05-14 16:30:18 +0300

    commit

--- MedicApp/Registration+Enter/Enteties/ButtonGradient.swift
+++ MedicApp/Registration+Enter/Enteties/ButtonGradient.swift
@@ -14,27 +14,48 @@ class ButtonGradient: UIButton {
     @IBInspectable var color1: UIColor = #colorLiteral(red: 0.9725490196, green: 0.3803921569, blue: 0.3568627451, alpha: 1)
     @IBInspectable var color2: UIColor = #colorLiteral(red: 0.9843137255, green: 0.6862745098, blue: 0.3764705882, alpha: 1)
     
-    let shadowView = UIView()
+//    let shadowView = UIView()
     let myImageView = UIImageView()
-    
+//
     var imageSet = false
-    
-    override var frame: CGRect {
-        didSet {
-            shadowView.frame = frame
-            setMyImage(bounds)
-        }
-    }
+//
+//    override var frame: CGRect {
+//        didSet {
+//            shadowView.frame = frame
+//            setMyImage(bounds)
+//        }
+//    }
     
     override func draw(_ rect: CGRect) {
         print("рисую")
-        setRounded()
-        addGradient(color1: color1, color2: color2)
-        setupShadow()
-        
+//        setRounded()
+//        addGradient(color1: color1, color2: color2)
+//        setupShadow()
+//
         if !imageSet {
             setMyImage(bounds)
         }
+        configureButNext()
+        
+    }
+    
+    
+    func configureButNext() {
+        print("Рамка кнопки некст: \(bounds)")
+        layer.cornerRadius = bounds.height/2
+        addGradient(colors: [color1.cgColor,
+                             color2.cgColor],
+                    coordinatesX: [1,0],
+                    coordinatesY: [0,0],
+                    cornerRadius: layer.cornerRadius)
+        
+        layer.shadowColor = #colorLiteral(red: 0.2509803922, green: 0.6980392157, blue: 0.9529411765, alpha: 1)
+        layer.shadowOpacity = 0.64
+        layer.shadowRadius = 10
+        let bounds = self.bounds
+        let insetedBounds = bounds.inset(by: UIEdgeInsets(top: 20, left: 10, bottom: -10, right: 10))
+        
+        layer.shadowPath = CGPath(rect: insetedBounds, transform: nil)
     }
     
     
@@ -59,17 +80,17 @@ class ButtonGradient: UIButton {
 //                                   end: endPoint,
 //                                   options: [])
         
-        let gradient = CAGradientLayer()
-        gradient.colors = [color1.cgColor, color2.cgColor]
-        let colorLocations: [NSNumber] = [0, 1]
-        let startPoint = CGPoint(x: 0, y: 0)
-        let endPoint = CGPoint(x: 1, y: 0)
-        gradient.startPoint = startPoint
-        gradient.endPoint = endPoint
-        gradient.locations = colorLocations
-        gradient.frame = bounds
-        layer.insertSublayer(gradient, below: layer)
-        print("Добавил градиент")
+//        let gradient = CAGradientLayer()
+//        gradient.colors = [color1.cgColor, color2.cgColor]
+//        let colorLocations: [NSNumber] = [0, 1]
+//        let startPoint = CGPoint(x: 0, y: 0)
+//        let endPoint = CGPoint(x: 1, y: 0)
+//        gradient.startPoint = startPoint
+//        gradient.endPoint = endPoint
+//        gradient.locations = colorLocations
+//        gradient.frame = bounds
+//        layer.insertSublayer(gradient, below: layer)
+//        print("Добавил градиент")
     }
     
     private func setRounded() {
@@ -78,15 +99,15 @@ class ButtonGradient: UIButton {
     }
     
     private func setupShadow() {
-        shadowView.frame = frame
-        shadowView.layer.cornerRadius = layer.cornerRadius
-        shadowView.layer.shadowOffset = CGSize(width: 0, height: 2)
-        shadowView.layer.shadowOpacity = 0.4
-        shadowView.layer.shadowColor = UIColor.black.cgColor
-        shadowView.layer.shadowRadius = 2
-        shadowView.backgroundColor = UIColor.white
-        superview!.insertSubview(shadowView, belowSubview: self)
-        print("Добавил тень кнопке")
+//        shadowView.frame = frame
+//        shadowView.layer.cornerRadius = layer.cornerRadius
+//        shadowView.layer.shadowOffset = CGSize(width: 0, height: 2)
+//        shadowView.layer.shadowOpacity = 0.4
+//        shadowView.layer.shadowColor = UIColor.black.cgColor
+//        shadowView.layer.shadowRadius = 2
+//        shadowView.backgroundColor = UIColor.white
+//        superview!.insertSubview(shadowView, belowSubview: self)
+//        print("Добавил тень кнопке")
     }
 
     private func setMyImage(_ rect: CGRect) {
@@ -99,7 +120,7 @@ class ButtonGradient: UIButton {
         myImageView.image = UIImage(named: "Стрелка вправо")
         myImageView.contentMode = .scaleAspectFit
         addSubview(myImageView)
-        
+
         imageSet = true
     }
 }
--- MedicApp/Registration+Enter/RegistrationVC/RegistrationView.swift
+++ MedicApp/Registration+Enter/RegistrationVC/RegistrationView.swift
@@ -24,26 +24,11 @@ class RegistrationView: UIViewController {
         turnViewsUpsideDown(viewWaveBottomMas)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        animateButGo()
-    }
-    
     
     private func turnViewsUpsideDown(_ views: [UIView]) {
         for view in views {
             view.transform = CGAffineTransform(rotationAngle: .pi)
         }
-    }
-    
-    private func animateButGo() {
-        
-        let radius = butGo.bounds.height / 2
-        
-        self.butGo.frame = CGRect(x: self.viewUnderTextFields.frame.maxX - radius,
-                                  y: self.viewUnderTextFields.center.y - radius,
-                                  width: self.butGo.frame.width,
-                                  height: self.butGo.frame.height)
-        
     }
 
     
